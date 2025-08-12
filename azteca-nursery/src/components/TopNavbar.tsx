58401605bbdb7895cd2a2977936342ce4924c95b
dhanielbolosan <dhaniel808123@gmail.com>
2025-08-11 23:49:51 -1000

    Convert to Offcanvas Navbar

--- azteca-nursery/src/components/TopNavbar.tsx
+++ azteca-nursery/src/components/TopNavbar.tsx
@@ -1,29 +1,32 @@
+'use client';
 
-import { Container, Nav, Navbar, NavbarBrand, NavLink, NavbarToggle, NavbarCollapse } from 'react-bootstrap';
 import React from 'react';
+import { Container, Nav, Navbar, NavLink, Offcanvas} from 'react-bootstrap';
 
 const TopNavbar: React.FC = () => {
     return (
-        <Navbar className="fixed-top custom-navbar" expand="lg">
+        <Navbar expand="lg" className="fixed-top custom-navbar">
             <Container className="position-relative">
-                <NavbarBrand
-                    href="/"
-                    className="position-absolute start-50 translate-middle-x"
-                >
-                    Azteca Nursery
-                </NavbarBrand>
-                <NavbarToggle aria-controls="basic-navbar-nav" className="ms-auto" />
-                <NavbarCollapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <NavLink href="/about-us">About Us</NavLink>
-                        <NavLink href="/catalog">Catalog</NavLink>
-                    </Nav>
-                    <Nav className="ms-auto">
-                        <NavLink href="/swap-meet">Swap Meet</NavLink>
-                        <NavLink href="/contact-us">Contact Us</NavLink>
-                        <NavLink href="/">EN/ES</NavLink>
-                    </Nav>
-                </NavbarCollapse>
+                <Navbar.Brand href="#" className="position-absolute start-50 translate-middle-x">Azteca Nursery</Navbar.Brand>
+                <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" className="d-lg-none ms-auto" />
+                <Navbar.Offcanvas className="custom-offcanvas" id="offcanvasNavbar-expand-lg" aria-labelledby="offcanvasNavbarLabel-expand-lg" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                            Azteca Nursery
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="me-auto">
+                            <NavLink href="/about-us">About Us</NavLink>
+                            <NavLink href="/catalog">Catalog</NavLink>
+                        </Nav>
+                        <Nav className="ms-auto">
+                            <NavLink href="/swap-meet">Swap Meet</NavLink>
+                            <NavLink href="/contact-us">Contact Us</NavLink>
+                            <NavLink href="/">EN/ES</NavLink>
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
             </Container>
         </Navbar>
     );
