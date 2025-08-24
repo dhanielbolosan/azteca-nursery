76fd379d710f0fc25dde779ef83506022249b53a
dhanielbolosan <dhaniel808123@gmail.com>
2025-08-23 21:49:51 -1000

    About Us Section + Removing Files

--- azteca-nursery/src/app/page.tsx
+++ azteca-nursery/src/app/page.tsx
@@ -1,8 +1,8 @@
-import { Container, Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <>
+      {/* Homepage */}
       <div id="home" className="homepage section">
         <div className="homepage-pic" id="pic3"/>
         <div className="homepage-pic" id="pic2"/>
@@ -16,13 +16,21 @@ export default function Home() {
           </Container>
         </div>
       </div>
-      <div id="about" className="section">
-        <Container>
-          <h2 style={{ fontFamily: "Pt Serif" }}>About Us</h2>
-          <p>This is the About Us section.</p>
-        </Container>
+
+      {/* About Us */}
+      <div id="about" className="about-us section">
+        <div className="about-content">
+          <Image className="about-image" src="/images/about-us-holder1.jpg" alt="Image of Azteca Nursery Owner" />
+          <div className="about-text">
+            <h2 className="about-title">About Us</h2>
+            <p className="about-text"> Lorem ipsum dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien vitae pellentesque sem placerat in id cursus mi pretium tellus duis convallis tempus leo eu aenean sed diam urna tempor pulvinar vivamus fringilla lacus nec metus bibendum egestas iaculis massa nisl malesuada lacinia integer nunc posuere ut hendrerit semper vel class aptent taciti sociosqu ad litora torquent per conubia nostra inceptos himenaeos orci varius natoque penatibus et magnis dis parturient montes nascetur ridiculus mus donec rhoncus eros lobortis nulla molestie mattis scelerisque maximus eget fermentum odio phasellus non purus est efficitur laoreet mauris pharetra vestibulum fusce dictum risus blandit quis suspendisse aliquet nisi sodales consequat magna ante condimentum neque at luctus nibh finibus facilisis dapibus etiam interdum tortor ligula congue sollicitudin erat viverra ac tincidunt nam porta elementum a enim euismod quam.
+            </p>
+          </div>
+        </div>
       </div>
 
+
+      {/* Catalog */}
       <div id="catalog" className="section">
         <Container>
           <h2>Catalog</h2>
@@ -30,6 +38,7 @@ export default function Home() {
         </Container>
       </div>
 
+      {/* Swap meet */}
       <div id="swap-meet" className="section">
         <Container>
           <h2>Swap Meet</h2>
@@ -37,6 +46,7 @@ export default function Home() {
         </Container>
       </div>
 
+      {/* Contact */}
       <div id="contact" className="section">
         <Container>
           <h2>Contact</h2>
